116bff8bcf0d4d6de9bcccd42d482f489e3b6331
Dichao Wang <68510103+DWMMI@users.noreply.github.com>
2023-06-04 22:53:13 +0200

    resposive con @media implementado

--- jscrip_login.js
+++ jscrip_login.js
@@ -3,10 +3,17 @@ function login() {
 
     user = document.getElementById("usuario").value;
     password = document.getElementById("contraseña").value;
-
+/*
+usuario: irene 
+contraseña: 1234
+*/
     if (user == "irene" && password == "1234") {
         window.location = "PaginaPrincipal.html";
     } else {
         alert("Datos incorrectos")
     }
+}
+
+//funcion sin implementar
+function registrar() {
 }
